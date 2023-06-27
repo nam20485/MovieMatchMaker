2309c69f614c2506a15f3dd465621bc2f6f4206d
Nathan <nmiller217@gmail.com>
2023-06-27 09:28:17 -0700

    add production member classes

---
+++ MovieMatchMakerLib/Production.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieMatchMakerLib
+{
+    public class Production
+    {
+        public Director Director { get; set; }     
+        //public Writer
+    }
+}
