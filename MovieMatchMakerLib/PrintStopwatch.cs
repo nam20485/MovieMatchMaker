4dab5b817541553ee644c082cb58c87a8d93e7eb
Nathan <nmiller217@gmail.com>
2023-07-13 18:12:23 -0700

    add custom Stopwatch class for convenient message printing

---
+++ MovieMatchMakerLib/PrintStopwatch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MovieMatchMakerLib
+{
+    public class PrintStopwatch : Stopwatch
+    {
+        public string MessageFormat { get; set; }
+
+        private const string _defaultMessageFormat = "{0} ({1} ms)";
+
+        public PrintStopwatch()
+            : this(_defaultMessageFormat)
+        { 
+        }
+
+        public PrintStopwatch(string messageFormat)
+            : base()
+        {
+            MessageFormat = messageFormat;
+        }
+
+        public void Start(string message, bool newLine = true)
+        {
+            if (newLine)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.Write(message);
+            }
+            Start();
+        }
+
+        public Stopwatch StartNew(string message, bool newLine = true)
+        {
+            if (newLine)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.Write(message);
+            }
+            return StartNew();
+        }
+
+        public void Stop(string message)
+        {
+            Stop();
+            Console.WriteLine(string.Format(MessageFormat, message, ElapsedMilliseconds));
+            Reset();
+        }
+    }
+}
