2826fd57702e4255b09733b155bf229675ae1558
Nathan <nmiller217@gmail.com>
2023-07-13 18:10:24 -0700

    change app from desktop to console

--- MovieMatchMakerApp00000/MainForm.cs
+++ MovieMatchMakerApp00000/MainForm.cs
@@ -1,39 +1,45 @@
-﻿using MovieMatchMakerLib.Filters;
-using MovieMatchMakerLib;
-using static System.Net.Mime.MediaTypeNames;
 using System.Diagnostics;
+
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+using MovieMatchMakerLib;
+using MovieMatchMakerLib.Filters;
 
 namespace MovieMatchMakerApp
 {
-    internal class Program
+    public partial class MainForm : Form
     {
         private const string Title = "Movie Match Maker";
-        private const string Slogan = "Find movies you didn't know you liked";
-        private const string Header = $"{Title} - {Slogan}!";
+        private const string Slogan = "Find movies you didn't know you liked";       
 
         private static string AppDataPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         private static string FilePath => Path.Combine(AppDataPath, "moviematchmaker.json");
         private static string MovieConnectionsFilePath => Path.Combine(AppDataPath, "movieconnections.json");
 
-        private static readonly MovieNetworkDataBuilder _movieNetworkDataBuilder;
-        private static readonly IDataCache _dataCache;
-        private static readonly MovieConnectionBuilder _connectionBuilder;
+        private readonly MovieNetworkDataBuilder _movieNetworkDataBuilder;
+        private readonly IDataCache _dataCache;
+        private MovieConnectionBuilder _connectionBuilder;
 
-        static Program()
+        public MainForm()
         {
             _dataCache = JsonFileCache.Load(FilePath);
             var apiDataSource = new ApiDataSource();
             var cachedDataSource = new CachedDataSource(_dataCache, apiDataSource);
             _movieNetworkDataBuilder = new MovieNetworkDataBuilder(cachedDataSource);
-            _connectionBuilder = new MovieConnectionBuilder(_dataCache);                        
-        }     
+            _connectionBuilder = new MovieConnectionBuilder(_dataCache);
 
-        static int Main(string[] args)
+            InitializeComponent();
+
+            Text = $"{Title} - {Slogan}!";
+        }
+
+        protected override async void OnLoad(EventArgs e)
         {
-            Console.WriteLine(Header);
-            Console.WriteLine(/* blank line for separation */);
+            base.OnLoad(e);
 
-            var stopWatch = new PrintStopwatch();
+            UseWaitCursor = true;
+
+            var stopWatch = new Stopwatch();
 
             //await _connectionManager.FindMoviesConnectedToMovie("Dark City", 1998, 1);
             //await _dataCache.SaveAsync();                                   
@@ -41,39 +47,23 @@ namespace MovieMatchMakerApp
             bool movieConnectionsLoaded = false;
             try
             {
-                stopWatch.Start("Loading movie connections from file... ", false);
-
                 _connectionBuilder.LoadMovieConnections(MovieConnectionsFilePath);
-                movieConnectionsLoaded = true;                
-
-                stopWatch.Stop("loaded");
+                movieConnectionsLoaded = true;
             }
             catch (Exception)
             {
-                Console.WriteLine("failed");
+                // do nothing
             }
 
             if (!movieConnectionsLoaded)
             {
-                stopWatch.Start("Finding movie connections... ", false);
-
-                _connectionBuilder.FindMovieConnections().Wait();
-
-                stopWatch.Stop("complete");
-
-                stopWatch.Start("Saving movie connections to file... ", false);
-
+                
+                await _connectionBuilder.FindMovieConnections();
                 _connectionBuilder.SaveMovieConnections(MovieConnectionsFilePath);
-
-                stopWatch.Stop("saved");
             }
-
-            Console.WriteLine($"Found {_connectionBuilder.MovieConnections.Count} movie connections");
 
             if (_connectionBuilder.MovieConnections.Count > 0)
             {
-                stopWatch.Start("Applying filters... ", false);
-
                 var sorted = new SortFilter().Apply(_connectionBuilder.MovieConnections);
                 var greaterThan2ConnectedRoles = new MinConnectedRolesCountFilter().Apply(_connectionBuilder.MovieConnections);
                 var greaterThan4ConnectedRoles = new MinConnectedRolesCountFilter(5).Apply(_connectionBuilder.MovieConnections);
@@ -87,13 +77,11 @@ namespace MovieMatchMakerApp
                     new MaxMatchingTitleWordsFilter().Apply(
                         new MinConnectedRolesCountFilter().Apply(
                             new SortFilter().Apply(_connectionBuilder.MovieConnections)));
-
-                stopWatch.Stop("finished");
-
-                return 0;
             }
 
-            return 1;
+            UseWaitCursor = false;
+
+            //Close();
         }
     }
 }
