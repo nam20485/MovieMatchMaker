f104edf120538ca754a0b8157f0c2ebeee877545
Nathan <nmiller217@gmail.com>
2023-07-13 18:42:58 -0700

    add IEquatable interface to ConnectedRoles and consider revsered jobs with all other fields equal to be the an equal ConnectedRole

--- MovieMatchMakerLib/ConnectedRole.cs
+++ MovieMatchMakerLib/ConnectedRole.cs
@@ -5,14 +5,44 @@ using System.Text;
 
 namespace MovieMatchMakerLib
 {
-    public class ConnectedRole
+    public class ConnectedRole : IEquatable<ConnectedRole>
     {
         public Name Name { get; set; }
         public int PersonId { get; set; }
         public string TargetJob { get; set; }
         public string SourceJob { get; set; }
 
-        public class StringDictionary : Dictionary<string, ConnectedRole>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ConnectedRole);
+        }
+
+        public bool Equals(ConnectedRole other)
+        {
+            return !(other is null) &&
+                   EqualityComparer<Name>.Default.Equals(Name, other.Name) &&
+                   PersonId == other.PersonId &&
+                   // allow for reversed jobs
+                   ((TargetJob == other.TargetJob && SourceJob == other.SourceJob) ||
+                    (TargetJob == other.SourceJob && SourceJob == other.TargetJob));
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, PersonId, TargetJob, SourceJob);
+        }
+
+        public static bool operator ==(ConnectedRole left, ConnectedRole right)
+        {
+            return EqualityComparer<ConnectedRole>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(ConnectedRole left, ConnectedRole right)
+        {
+            return !(left == right);
+        }  
+
+        public class NameDictionary : Dictionary<Name, ConnectedRole>
         {
         }
 
