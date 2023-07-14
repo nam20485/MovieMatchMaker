67aa680bb08b975a59c0641437d755d82ce34cde
Nathan <nmiller217@gmail.com>
2023-07-14 10:50:21 -0700

    add SystemFolders class

---
+++ MovieMatchMakerLib/Utils/SystemFolders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MovieMatchMakerLib.Utils
+{
+    public class SystemFolders
+    {
+        public static string AppDataPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    }
+}
