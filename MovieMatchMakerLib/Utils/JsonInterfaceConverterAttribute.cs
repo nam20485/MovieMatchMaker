93fea43dca4b9ccceb3f6927b2da262e3570def3
Nathan <nmiller217@gmail.com>
2023-07-15 13:34:33 -0700

    classes for serializing derived types of interfaces correctly

---
+++ MovieMatchMakerLib/Utils/JsonInterfaceConverterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MovieMatchMakerLib.Utils
+{
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+    public class JsonInterfaceConverterAttribute : JsonConverterAttribute
+    {
+        public JsonInterfaceConverterAttribute(Type converterType)
+            : base(converterType)
+        {
+            // do nothing
+        }
+    }
+}
