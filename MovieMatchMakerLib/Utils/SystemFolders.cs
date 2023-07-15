86a7f1651390fcce49008929b26ef54ba6fe748a
Nathan <nmiller217@gmail.com>
2023-07-15 09:39:43 -0700

    minor refactor

--- MovieMatchMakerLib/Utils/SystemFolders.cs
+++ MovieMatchMakerLib/Utils/SystemFolders.cs
@@ -4,6 +4,6 @@ namespace MovieMatchMakerLib.Utils
 {
     public class SystemFolders
     {
-        public static string AppDataPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public static string LocalAppDataPath => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     }
 }
