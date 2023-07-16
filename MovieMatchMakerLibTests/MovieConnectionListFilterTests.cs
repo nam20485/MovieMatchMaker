c711465948ab01652d904ebc05f879e25e2aad2d
Nathan <nmiller217@gmail.com>
2023-07-15 19:10:36 -0700

    add tests for serialization/deserialization of derived types in an interface-typed collection

--- MovieMatchMakerLibTests/MovieConnectionListFilterTests.cs
+++ MovieMatchMakerLibTests/MovieConnectionListFilterTests.cs
@@ -8,7 +8,8 @@ namespace MovieMatchMakerLibTests
 {
     public class MovieConnectionListFilterTests
     {
-        private const string ExpectedSerializedJson = "";
+        private const string ExpectedDerivedTypesInInterfaceTypedListSerializedJson = "[{\"$type\":\"Sort\",\"SortOrder\":1},{\"$type\":\"MaxMatchingTitleWords\",\"MaxMatchingTitleWords\":10},{\"$type\":\"MinConnectedRolesCount\",\"MinConnectedRolesCount\":10}]";
+        private const string ExpectedDerivedTypeListSerializedJson = "[{\"MaxMatchingTitleWords\":1},{\"MaxMatchingTitleWords\":2},{\"MaxMatchingTitleWords\":3}]";        
 
         private readonly IMovieConnectionListFilter[] DerivedTypesInInterfaceTypedList = new IMovieConnectionListFilter[]
         { 
@@ -17,50 +18,87 @@ namespace MovieMatchMakerLibTests
             new MinConnectedRolesCountFilter(10),
         };
 
-    [Fact]
-        public void Test_DerivedTypesInInterfaceTypedSerialization()
+        private readonly MaxMatchingTitleWordsFilter[] DerivedTypeList = new []
+        {
+            new MaxMatchingTitleWordsFilter(1),
+            new MaxMatchingTitleWordsFilter(2),
+            new MaxMatchingTitleWordsFilter(3),
+        };
+
+        [Fact]
+        public void Test_DerivedTypeListSerialization()
+        {
+            var json = JsonSerializer.Serialize(DerivedTypeList);
+            json.Should().NotBeNull();
+            json.Should().NotBeEmpty();
+            json.Should().BeEquivalentTo(ExpectedDerivedTypeListSerializedJson);
+        }
+
+        [Fact]
+        public void Test_DerivedTypeListDeserialization()
+        {
+            var deserializedList = JsonSerializer.Deserialize<MaxMatchingTitleWordsFilter[]>(ExpectedDerivedTypeListSerializedJson);
+            deserializedList.Should().NotBeNull();
+            deserializedList.Should().NotBeEmpty();
+            deserializedList.Should().BeEquivalentTo(DerivedTypeList);
+        }
+
+        [Fact]
+        public void Test_DerivedTypesInInterfaceTypedListSerialization()
         {            
             var json = JsonSerializer.Serialize(DerivedTypesInInterfaceTypedList);
             json.Should().NotBeNull();
             json.Should().NotBeEmpty();
-            json.Should().BeEquivalentTo(ExpectedSerializedJson);
+            json.Should().BeEquivalentTo(ExpectedDerivedTypesInInterfaceTypedListSerializedJson);
         }
 
         [Fact]
-        public void Test_DerivedTypesInInterfaceTypedDeserialization()
+        public void Test_DerivedTypesInInterfaceTypedListDeserialization()
         {
-            var deserializedList = JsonSerializer.Deserialize<IMovieConnectionListFilter[]>(ExpectedSerializedJson);
+            var deserializedList = JsonSerializer.Deserialize<IMovieConnectionListFilter[]>(ExpectedDerivedTypesInInterfaceTypedListSerializedJson);
             deserializedList.Should().NotBeNull();
             deserializedList.Should().NotBeEmpty();
-            deserializedList.Should().BeEquivalentTo(DerivedTypesInInterfaceTypedList);
+            deserializedList.Should().HaveCount(3);
+            deserializedList.Should().BeOfType(typeof(IMovieConnectionListFilter[]));
+            deserializedList![0].Should().BeOfType(typeof(SortFilter));
+            deserializedList[1].Should().BeOfType(typeof(MaxMatchingTitleWordsFilter));
+            deserializedList[2].Should().BeOfType(typeof(MinConnectedRolesCountFilter));
         }
 
         [Fact]
-        public void Test_DerivedTypesInInterfaceTypedRoundTripSerialization()
+        public void Test_DerivedTypesInInterfaceTypedListRoundTripSerialization()
         {
             var serializedJson = JsonSerializer.Serialize(DerivedTypesInInterfaceTypedList);
             serializedJson.Should().NotBeNull();
             serializedJson.Should().NotBeEmpty();
-            serializedJson.Should().BeEquivalentTo(ExpectedSerializedJson);
+            serializedJson.Should().BeEquivalentTo(ExpectedDerivedTypesInInterfaceTypedListSerializedJson);
 
             var deserializedList = JsonSerializer.Deserialize<IMovieConnectionListFilter[]>(serializedJson);
             deserializedList.Should().NotBeNull();
             deserializedList.Should().NotBeEmpty();
-            deserializedList.Should().BeEquivalentTo(DerivedTypesInInterfaceTypedList);
+            deserializedList.Should().HaveCount(3);
+            deserializedList.Should().BeOfType(typeof(IMovieConnectionListFilter[]));
+            deserializedList![0].Should().BeOfType(typeof(SortFilter));
+            deserializedList[1].Should().BeOfType(typeof(MaxMatchingTitleWordsFilter));
+            deserializedList[2].Should().BeOfType(typeof(MinConnectedRolesCountFilter));
         }
 
         [Fact]
-        public void Test_DerivedTypesInInterfaceTypedRoundTripDeserialization()
+        public void Test_DerivedTypesInInterfaceTypedListRoundTripDeserialization()
         {
-            var deserializedList = JsonSerializer.Deserialize<IMovieConnectionListFilter[]>(ExpectedSerializedJson);
+            var deserializedList = JsonSerializer.Deserialize<IMovieConnectionListFilter[]>(ExpectedDerivedTypesInInterfaceTypedListSerializedJson);
             deserializedList.Should().NotBeNull();
             deserializedList.Should().NotBeEmpty();
-            deserializedList.Should().BeEquivalentTo(DerivedTypesInInterfaceTypedList);
+            deserializedList.Should().HaveCount(3);
+            deserializedList.Should().BeOfType(typeof(IMovieConnectionListFilter[]));
+            deserializedList![0].Should().BeOfType(typeof(SortFilter));
+            deserializedList[1].Should().BeOfType(typeof(MaxMatchingTitleWordsFilter));
+            deserializedList[2].Should().BeOfType(typeof(MinConnectedRolesCountFilter));
 
             var serializedJson = JsonSerializer.Serialize(deserializedList);
             serializedJson.Should().NotBeNull();
             serializedJson.Should().NotBeEmpty();
-            serializedJson.Should().BeEquivalentTo(ExpectedSerializedJson);            
+            serializedJson.Should().BeEquivalentTo(ExpectedDerivedTypesInInterfaceTypedListSerializedJson);            
         }
     }
 }
