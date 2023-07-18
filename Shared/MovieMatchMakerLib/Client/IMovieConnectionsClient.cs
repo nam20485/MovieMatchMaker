d930e02f666f315c10b8feb6e80a552824ceb352
Nathan <nmiller217@gmail.com>
2023-07-18 07:14:15 -0700

    add MovieConnetionsApiClient and interface for fetching MovieConnections from the backend

---
+++ Shared/MovieMatchMakerLib/Client/IMovieConnectionsClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MovieMatchMakerLib.Filters;
+using MovieMatchMakerLib.Model;
+
+namespace MovieMatchMakerLib.Client
+{
+    public interface IMovieConnectionsClient
+    {
+        Task<MovieConnection.List> GetAllMovieConnections();
+        Task<MovieConnection.List> GetMovieConnectionsForMovie(string title, int releaseYear);
+
+        Task<MovieConnection.List> FilterAllMovieConnections(IEnumerable<IMovieConnectionListFilter> filters);
+
+        Task<MovieConnection.List> FilterMovieConnectionsForMovie(string title, int releaseYear, IEnumerable<IMovieConnectionListFilter> filters);
+
+    }
+}
---
+++ Shared/MovieMatchMakerLib/Client/MovieConnectionsApiClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+using MovieMatchMakerLib.Filters;
+using MovieMatchMakerLib.Model;
+using MovieMatchMakerLib.Utils;
+
+namespace MovieMatchMakerLib.Client
+{
+    public class MovieConnectionsApiClient : IMovieConnectionsClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public MovieConnectionsApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<MovieConnection.List> GetAllMovieConnections()
+        {
+            //try
+            {
+                return await _httpClient.GetFromJsonAsync<MovieConnection.List>("MovieConnections/movieconnections", MyJsonSerializerOptions.JsonSerializerOptions);
+            }
+            //catch (Exception ex)
+            {
+                //    Console.WriteLine(ex);
+            }
+        }
+
+        public Task<MovieConnection.List> GetMovieConnectionsForMovie(string title, int releaseYear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovieConnection.List> FilterAllMovieConnections(IEnumerable<IMovieConnectionListFilter> filters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MovieConnection.List> FilterMovieConnectionsForMovie(string title, int releaseYear, IEnumerable<IMovieConnectionListFilter> filters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
