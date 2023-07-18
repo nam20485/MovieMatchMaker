58b76a11598eb1508c59232d1a25b30ae131b458
Nathan <nmiller217@gmail.com>
2023-07-18 08:05:46 -0700

    read logging settings and ApiUrl from appsettings.json

--- Frontend/MovieMatchMakerClient/Program.cs
+++ Frontend/MovieMatchMakerClient/Program.cs
@@ -10,14 +10,24 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// configure logging from appsettings.json
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // MudBlazor
 builder.Services.AddMudServices();
 
+var apiUrl = builder.Configuration["ApiUrl"];
+//if (string.IsNullOrWhiteSpace(apiUrl))
+//{
+//    // hard-coded default
+//    apiUrl = "https://localhost:7288/api/";
+//}
+
 builder.Services.AddHttpClient<IMovieConnectionsClient, MovieConnectionsApiClient>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7288/api/");
+    client.BaseAddress = new Uri(apiUrl);
 });
 
 await builder.Build().RunAsync();
