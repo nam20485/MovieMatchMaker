524b0495d9c0bcd5b38096dd464d68e5c3b700b8
Nathan <nmiller217@gmail.com>
2023-07-18 14:34:03 -0700

    minor refactor

--- Shared/MovieMatchMakerLib/Model/MovieConnection.cs
+++ Shared/MovieMatchMakerLib/Model/MovieConnection.cs
@@ -84,11 +84,6 @@ namespace MovieMatchMakerLib.Model
                         {
                             movies.Add(connection.TargetMovie);
                         }
-                        //if (!movies.Exists(m => m.Title == connection.SourceMovie.Title && m.ReleaseYear == connection.SourceMovie.ReleaseYear))                        
-                        //if (! movies.Contains(connection.TargetMovie))
-                        //{
-                        //    movies.Add(connection.TargetMovie);
-                        //}
                     }
                     return movies;
                 }
