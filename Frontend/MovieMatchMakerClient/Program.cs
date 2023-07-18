fc23c8d902c95c0edad932a7aa04c6eda9c4fe34
Nathan <nmiller217@gmail.com>
2023-07-18 07:15:09 -0700

    register MovieConnectionsApiClient service for DI

--- Frontend/MovieMatchMakerClient/Program.cs
+++ Frontend/MovieMatchMakerClient/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using MovieMatchMakerClient;
-
+using MovieMatchMakerLib.Client;
 using MudBlazor.Services;
 
 
@@ -10,9 +10,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // MudBlazor
 builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient<IMovieConnectionsClient, MovieConnectionsApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7288/api/");
+});
 
 await builder.Build().RunAsync();
