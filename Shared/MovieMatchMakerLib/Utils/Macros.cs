a676282125f2d732551b5e856eb27027981e6591
Nathan <nmiller217@gmail.com>
2023-07-19 18:46:49 -0700

    add function to determine if in debug build

---
+++ Shared/MovieMatchMakerLib/Utils/Macros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieMatchMakerLib.Utils
+{
+    public static class Macros
+    {
+        public static bool IsDebug()
+        {
+            #if DEBUG
+                return true;
+            #else
+                return false;
+            #endif
+        }
+    }
+}
