d200c7bf6154563c2864b25bcbb48cd351d35fd6
Nathan <nmiller217@gmail.com>
2023-07-22 13:08:06 -0700

    minor changes

--- Shared/MovieMatchMakerLib/Constants.cs
+++ Shared/MovieMatchMakerLib/Constants.cs
@@ -5,8 +5,8 @@
         public static class Strings
         {
             public const string Title = "Movie Match Maker";
-            public const string Slogan = "Find movies you didn't know you liked";
-            public readonly static string Header = $"{Title} - {Slogan}!";
+            public const string Slogan = "Find movies you didn't know you liked!";
+            public readonly static string Header = $"{Title} - {Slogan}";
         }
     }
 }
