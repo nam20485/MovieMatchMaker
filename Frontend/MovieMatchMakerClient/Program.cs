d7d6a935149435faed590c5210d45f60d3a8c6fe
Nathan <nmiller217@gmail.com>
2023-07-22 13:04:58 -0700

    add js

--- Frontend/MovieMatchMakerClient/Program.cs
+++ Frontend/MovieMatchMakerClient/Program.cs
@@ -13,7 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // configure logging from appsettings.json
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // MudBlazor
 builder.Services.AddMudServices();
