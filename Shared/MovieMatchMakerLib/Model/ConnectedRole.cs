8c63db247d1416335d475b8814d311b0c2338254
Nathan <nmiller217@gmail.com>
2023-07-27 22:15:42 -0700

    rename folder and move class to folder

--- Shared/MovieMatchMakerLib/Model/ConnectedRole.cs
+++ Shared/MovieMatchMakerLib/Model/ConnectedRole.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MovieMatchMakerLib.TmdbApi;
 
 namespace MovieMatchMakerLib.Model
 {
--- Shared/MovieMatchMakerLib/Model/Movie.cs
+++ Shared/MovieMatchMakerLib/Model/Movie.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-
+using MovieMatchMakerLib.TmdbApi;
 
 namespace MovieMatchMakerLib.Model
 {
