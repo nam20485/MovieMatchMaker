eaf87c00d800268f6f33b845fffe6517cc6d906a
Nathan <nmiller217@gmail.com>
2023-07-29 16:25:13 -0700

    minor changes

--- Shared/MovieMatchMakerLib/TmdbApi/ITmdbApi.cs
+++ Shared/MovieMatchMakerLib/TmdbApi/ITmdbApi.cs
@@ -6,15 +6,11 @@ using TMDbLib.Objects.People;
 namespace MovieMatchMakerLib.TmdbApi
 {
     public interface ITmdbApi
-    {
-        Task<ProfileImages> FetchImageDataForPerson(int personId);
-
-        //string TmdbApiKey { get; }
-
+    {             
         Task<Model.Movie> FetchMovieAsync(string title, int releaseYear);
         Task<Credits> FetchMovieCreditsAsync(string title, int releaseYear);
         Task<Credits> FetchMovieCreditsAsync(int movieApiId);
         Task<MovieCredits> FetchMovieCreditsForPerson(int personApiId);
-        //Task<Movie> FetchConnectedMovies(string title, int releaseYear);
+        Task<ProfileImages> FetchImageDataForPerson(int personId);
     }
 }
