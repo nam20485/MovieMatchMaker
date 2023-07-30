1af98c28ecc020478da6a31bdfa86960c7c9ca7a
Nathan <nmiller217@gmail.com>
2023-07-29 19:30:04 -0700

    add retry 5 times for API requests

--- Shared/MovieMatchMakerLib/TmdbApi/TmdbLibApi.cs
+++ Shared/MovieMatchMakerLib/TmdbApi/TmdbLibApi.cs
@@ -12,6 +12,7 @@ namespace MovieMatchMakerLib.TmdbApi
     public class TmdbLibApi : ITmdbApi
     {
         private readonly TMDbClient _apiClient;
+        private const int RetryCount = 5;
 
         public TmdbLibApi(string apiKey)
         {
@@ -20,21 +21,25 @@ namespace MovieMatchMakerLib.TmdbApi
 
         public async Task<Credits> FetchMovieCreditsAsync(string title, int releaseYear)
         {
-            try
+            var retryCount = RetryCount;
+            while (retryCount-- > 0)
             {
-                var movie = await FetchMovieAsync(title, releaseYear);
-                if (movie != null)
+                try
                 {
-                    var credits = await _apiClient.GetMovieCreditsAsync(movie.MovieId);
-                    if (credits != null)
+                    var movie = await FetchMovieAsync(title, releaseYear);
+                    if (movie != null)
                     {
-                        return credits;
+                        var credits = await _apiClient.GetMovieCreditsAsync(movie.MovieId);
+                        if (credits != null)
+                        {
+                            return credits;
+                        }
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
 
             return null;
@@ -44,32 +49,36 @@ namespace MovieMatchMakerLib.TmdbApi
         {
             Model.Movie movie = null;
 
-            try
+            var retryCount = RetryCount;
+            while (retryCount-- > 0)
             {
-                var searchResult = await _apiClient.SearchMovieAsync(title, primaryReleaseYear: releaseYear);
-                // handle when multiple results are returned b/c they have the title as a keyword
-                var movieResult = searchResult.Results.FirstOrDefault(r => r.Title == title && r.ReleaseDate.HasValue && r.ReleaseDate.Value.Year == releaseYear);
-
-                //SearchMovie result = null;
-                //if (searchResult.TotalResults == 1)
-                //{
-                //    result = searchResult.Results.First();
-                //}
-                //else if (searchResult.TotalResults > 1)
-                //{
-                //    result = searchResult.Results.FirstOrDefault(r => r.Title == title && r.ReleaseDate.HasValue && r.ReleaseDate.Value.Year == releaseYear);
-                //}
-
-                if (movieResult is not null)
+                try
                 {
-                    var movieId = movieResult.Id;
-                    var posterImagePath = movieResult.PosterPath;
-                    movie = new Model.Movie(title, releaseYear, movieId, posterImagePath);
+                    var searchResult = await _apiClient.SearchMovieAsync(title, primaryReleaseYear: releaseYear);
+                    // handle when multiple results are returned b/c they have the title as a keyword
+                    var movieResult = searchResult.Results.FirstOrDefault(r => r.Title == title && r.ReleaseDate.HasValue && r.ReleaseDate.Value.Year == releaseYear);
+
+                    //SearchMovie result = null;
+                    //if (searchResult.TotalResults == 1)
+                    //{
+                    //    result = searchResult.Results.First();
+                    //}
+                    //else if (searchResult.TotalResults > 1)
+                    //{
+                    //    result = searchResult.Results.FirstOrDefault(r => r.Title == title && r.ReleaseDate.HasValue && r.ReleaseDate.Value.Year == releaseYear);
+                    //}
+
+                    if (movieResult is not null)
+                    {
+                        var movieId = movieResult.Id;
+                        var posterImagePath = movieResult.PosterPath;
+                        movie = new Model.Movie(title, releaseYear, movieId, posterImagePath);
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
 
             return movie;
@@ -77,39 +86,51 @@ namespace MovieMatchMakerLib.TmdbApi
 
         public async Task<MovieCredits> FetchMovieCreditsForPerson(int personApiId)
         {
-            try
+            var retryCount = RetryCount;
+            while (retryCount-- > 0)
             {
-                return await _apiClient.GetPersonMovieCreditsAsync(personApiId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                try
+                {
+                    return await _apiClient.GetPersonMovieCreditsAsync(personApiId);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
             return null;
         }
 
         public async Task<Credits> FetchMovieCreditsAsync(int movieApiId)
         {
-            try
+            var retryCount = RetryCount;
+            while (retryCount-- > 0)
             {
-                return await _apiClient.GetMovieCreditsAsync(movieApiId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                try
+                {
+                    return await _apiClient.GetMovieCreditsAsync(movieApiId);                 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
             return null;
         }
 
         public async Task<ProfileImages> FetchImageDataForPerson(int personId)
         {
-            try
+            var retryCount = RetryCount;
+            while (retryCount-- > 0)
             {
-                return await _apiClient.GetPersonImagesAsync(personId);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
+                try
+                {
+                    return await _apiClient.GetPersonImagesAsync(personId);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
             return null;
         }
