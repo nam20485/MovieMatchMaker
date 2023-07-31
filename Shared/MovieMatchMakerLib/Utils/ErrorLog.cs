e1826f7e3caf68c058338340193f0c4dea1c8760
Nathan <nmiller217@gmail.com>
2023-07-31 12:31:19 -0700

    add basic errorlog class

---
+++ Shared/MovieMatchMakerLib/Utils/ErrorLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace MovieMatchMakerLib.Utils
+{
+    public static class ErrorLog
+    {
+        public const string ErrorLogFile = "error.txt";
+
+        private static readonly object _writeLock = new();
+
+        public static void Log(string message)
+        {
+            lock (_writeLock)
+            {
+                File.AppendAllText(ErrorLogFile, message);
+            }
+        }
+
+        public static void Log(Exception e)
+        {
+            Log(e.ToString());
+        }
+
+        public static void Reset()
+        {
+            File.Delete(ErrorLogFile);
+        }
+    }
+}
