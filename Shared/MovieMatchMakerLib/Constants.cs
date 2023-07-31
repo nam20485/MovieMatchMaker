65239a4f1756bb0e04d6d9cd82fb54649eafe354
Nathan <nmiller217@gmail.com>
2023-07-31 12:30:40 -0700

    add HeaderWithVersion

--- Shared/MovieMatchMakerLib/Constants.cs
+++ Shared/MovieMatchMakerLib/Constants.cs
@@ -7,6 +7,8 @@
             public const string Title = "Movie Match Maker";
             public const string Slogan = "Find movies you didn't know you liked!";
             public readonly static string Header = $"{Title} - {Slogan}";
+            public const string Version = "0.1";
+            public readonly static string HeaderWithVersion = $"{Title} - {Slogan} (v{Version})";
         }
     }
 }
