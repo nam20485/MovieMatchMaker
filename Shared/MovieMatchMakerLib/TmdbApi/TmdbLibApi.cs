fb967d7646f211fa4a460c0d74c240d202c495e6
Nathan <nmiller217@gmail.com>
2023-07-31 12:40:36 -0700

    add retries with exponential backoff delay to Tmdb API

--- Shared/MovieMatchMakerLib/TmdbApi/TmdbLibApi.cs
+++ Shared/MovieMatchMakerLib/TmdbApi/TmdbLibApi.cs
@@ -1,72 +1,49 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
+using MovieMatchMakerLib.Utils;
+
 using TMDbLib.Client;
+using TMDbLib.Objects.Exceptions;
 using TMDbLib.Objects.Movies;
 using TMDbLib.Objects.People;
-using TMDbLib.Objects.Search;
+
 
 namespace MovieMatchMakerLib.TmdbApi
 {
     public class TmdbLibApi : ITmdbApi
     {
-        private readonly TMDbClient _apiClient;
         private const int RetryCount = 5;
 
+        private readonly TMDbClient _apiClient;
+
+        private const int InitialTimeoutDelayMs = 100;        
+     
         public TmdbLibApi(string apiKey)
         {
             _apiClient = new TMDbClient(apiKey);
-        }
-
-        public async Task<Credits> FetchMovieCreditsAsync(string title, int releaseYear)
-        {
-            var retryCount = RetryCount;
-            while (retryCount-- > 0)
-            {
-                try
-                {
-                    var movie = await FetchMovieAsync(title, releaseYear);
-                    if (movie != null)
-                    {
-                        var credits = await _apiClient.GetMovieCreditsAsync(movie.MovieId);
-                        if (credits != null)
-                        {
-                            return credits;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-            }
-
-            return null;
-        }
+        }       
 
         public async Task<Model.Movie> FetchMovieAsync(string title, int releaseYear)
         {
             Model.Movie movie = null;
 
-            var retryCount = RetryCount;
-            while (retryCount-- > 0)
+            var timeoutDelay = InitialTimeoutDelayMs;
+            var retryCount = 0;
+            while (retryCount++ < RetryCount)
             {
                 try
                 {
                     var searchResult = await _apiClient.SearchMovieAsync(title, primaryReleaseYear: releaseYear);
                     // handle when multiple results are returned b/c they have the title as a keyword
-                    var movieResult = searchResult.Results.FirstOrDefault(r => r.Title == title && r.ReleaseDate.HasValue && r.ReleaseDate.Value.Year == releaseYear);
-
-                    //SearchMovie result = null;
-                    //if (searchResult.TotalResults == 1)
-                    //{
-                    //    result = searchResult.Results.First();
-                    //}
-                    //else if (searchResult.TotalResults > 1)
-                    //{
-                    //    result = searchResult.Results.FirstOrDefault(r => r.Title == title && r.ReleaseDate.HasValue && r.ReleaseDate.Value.Year == releaseYear);
-                    //}
+                    var movieResult = searchResult.Results.FirstOrDefault(r =>
+                    {                        
+                        return r.Title == title && 
+                               r.ReleaseDate.HasValue &&
+                               r.ReleaseDate.Value.Year == releaseYear;
+                    });
 
                     if (movieResult is not null)
                     {
@@ -75,61 +52,156 @@ namespace MovieMatchMakerLib.TmdbApi
                         movie = new Model.Movie(title, releaseYear, movieId, posterImagePath);
                     }
                 }
+                catch (TaskCanceledException tce)
+                {
+                    ErrorLog.Log(tce);
+                    if (tce.InnerException is TimeoutException)
+                    {
+                        // request timed out
+                    }
+                }
+                catch (RequestLimitExceededException rlee)
+                {
+                    ErrorLog.Log(rlee);
+                    Thread.Sleep(Convert.ToInt32(timeoutDelay * Math.Pow(2, retryCount)));
+                }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);
+                    ErrorLog.Log(ex);
                 }
             }
 
             return movie;
         }
 
-        public async Task<MovieCredits> FetchMovieCreditsForPerson(int personApiId)
+        public async Task<Credits> FetchMovieCreditsAsync(string title, int releaseYear)
         {
-            var retryCount = RetryCount;
-            while (retryCount-- > 0)
+            var timeoutDelay = InitialTimeoutDelayMs;
+            var retryCount = 0;
+            while (retryCount++ < RetryCount)
             {
                 try
                 {
-                    return await _apiClient.GetPersonMovieCreditsAsync(personApiId);
+                    var movie = await FetchMovieAsync(title, releaseYear);
+                    if (movie != null)
+                    {
+                        var credits = await _apiClient.GetMovieCreditsAsync(movie.MovieId);
+                        if (credits != null)
+                        {                            
+                            return credits;
+                        }
+                    }
+                }
+                catch (TaskCanceledException tce)
+                {
+                    ErrorLog.Log(tce);
+                    if (tce.InnerException is TimeoutException)
+                    {
+                        // request timed out
+                    }
+                }
+                catch (RequestLimitExceededException rlee)
+                {
+                    ErrorLog.Log(rlee);
+                    Thread.Sleep(Convert.ToInt32(timeoutDelay * Math.Pow(2, retryCount)));
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);
+                    ErrorLog.Log(ex);
                 }
             }
+
             return null;
         }
 
         public async Task<Credits> FetchMovieCreditsAsync(int movieApiId)
         {
-            var retryCount = RetryCount;
-            while (retryCount-- > 0)
+            var timeoutDelay = InitialTimeoutDelayMs;
+            var retryCount = 0;
+            while (retryCount++ < RetryCount)
             {
                 try
                 {
-                    return await _apiClient.GetMovieCreditsAsync(movieApiId);                 
+                    return await _apiClient.GetMovieCreditsAsync(movieApiId);
+                }
+                catch (TaskCanceledException tce)
+                {
+                    ErrorLog.Log(tce);
+                    if (tce.InnerException is TimeoutException)
+                    {
+                        // request timed out
+                    }
+                }
+                catch (RequestLimitExceededException rlee)
+                {
+                    ErrorLog.Log(rlee);
+                    Thread.Sleep(Convert.ToInt32(timeoutDelay * Math.Pow(2, retryCount)));
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);
+                    ErrorLog.Log(ex);
                 }
             }
             return null;
         }
 
+        public async Task<MovieCredits> FetchMovieCreditsForPerson(int personApiId)
+        {
+            var timeoutDelay = InitialTimeoutDelayMs;
+            var retryCount = 0;
+            while (retryCount++ < RetryCount)
+            {
+                try
+                {
+                    return await _apiClient.GetPersonMovieCreditsAsync(personApiId);
+                }
+                catch (TaskCanceledException tce)
+                {
+                    ErrorLog.Log(tce);
+                    if (tce.InnerException is TimeoutException)
+                    {
+                        // request timed out
+                    }
+                }
+                catch (RequestLimitExceededException rlee)
+                {
+                    ErrorLog.Log(rlee);
+                    Thread.Sleep(Convert.ToInt32(timeoutDelay * Math.Pow(2, retryCount)));
+                }
+                catch (Exception ex)
+                {
+                    ErrorLog.Log(ex);
+                }
+            }
+            return null;
+        }       
+
         public async Task<ProfileImages> FetchImageDataForPerson(int personId)
         {
-            var retryCount = RetryCount;
-            while (retryCount-- > 0)
+            var timeoutDelay = InitialTimeoutDelayMs;
+            var retryCount = 0;
+            while (retryCount++ < RetryCount)
             {
                 try
                 {
                     return await _apiClient.GetPersonImagesAsync(personId);
                 }
+                catch (TaskCanceledException tce)
+                {
+                    ErrorLog.Log(tce);
+                    if (tce.InnerException is TimeoutException)
+                    {
+                        // request timed out
+                    }
+                }
+                catch (RequestLimitExceededException rlee)
+                {
+                    ErrorLog.Log(rlee);
+                    Thread.Sleep(Convert.ToInt32(timeoutDelay * Math.Pow(2, retryCount)));
+                }
                 catch (Exception ex)
                 {
-                    Console.WriteLine(ex);
+                    ErrorLog.Log(ex);
                 }
             }
             return null;
