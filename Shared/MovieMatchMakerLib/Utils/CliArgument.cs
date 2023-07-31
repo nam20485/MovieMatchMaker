b624bb82864de7f4207004d7d2ab776b4d8c64cc
Nathan <nmiller217@gmail.com>
2023-07-31 15:59:45 -0700

    add CLI argument parsing classes

---
+++ Shared/MovieMatchMakerLib/Utils/CliArgument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieMatchMakerLib.Utils
+{
+    public class CliArgument<TType>
+    {
+        public string Name { get; set; }
+        public TType Value { get; set; }
+    }
+}
