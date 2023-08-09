4ab2857792446876dae88772682fedc8ad38e191
Nathan <nmiller217@gmail.com>
2023-08-09 10:50:04 -0700

    add model new class for identifying movies so to make use of builtin input validation

---
+++ Shared/MovieMatchMakerLib/Model/MovieIdentifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieMatchMakerLib.Model
+{
+    public class MovieIdentifier
+    {
+        [Required]
+        [StringLength(200)]
+        [RegularExpression("[A-Za-z0-9\\p{L}\\p{N} ]+")]   // > 0 Ascii letters & numbers, Unicode numbers and letters and space only
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1888, 2023)]
+        public int ReleaseYear { get; set; }
+
+        public MovieIdentifier()
+        {
+            // required for Model binding
+        }
+
+        public MovieIdentifier(string title, int releaseYear)
+        {            
+            Title = title;
+            ReleaseYear = releaseYear;
+        }
+
+    }
+}
