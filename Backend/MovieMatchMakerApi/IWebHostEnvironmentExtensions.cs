9d1363941e4ff05b3755cc2d66e01af2fdac1b6e
Nathan <nmiller217@gmail.com>
2023-08-11 12:04:12 -0700

    add extension

---
+++ Backend/MovieMatchMakerApi/IWebHostEnvironmentExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieMatchMakerApi
+{
+    public static class IWebHostEnvironmentExtensions
+    {
+        public static string MapPath(this IWebHostEnvironment instance, string path)
+        {
+            return Path.Combine(instance.WebRootPath, path);
+        }
+    }
+}
