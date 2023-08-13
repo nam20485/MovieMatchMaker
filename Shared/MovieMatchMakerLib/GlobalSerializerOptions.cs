81bf5cbb9e35a2850b5c344db885c95e825aff24
Nathan Miller <nmiller217@gmail.com>
2023-08-13 15:29:42 -0700

    move class

---
+++ Shared/MovieMatchMakerLib/GlobalSerializerOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using MovieMatchMakerLib.Utils;
+
+namespace MovieMatchMakerLib
+{
+    internal class GlobalSerializerOptions
+    {
+        internal static readonly JsonSerializerOptions Options;
+
+        static GlobalSerializerOptions()
+        {
+            Options = new ()
+            {
+                // disable pretty-printing in release builds to save file size and xfer speed
+                WriteIndented = Macros.IsDebugBuild(),
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true,
+                //DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,                
+                // TODO: why does this need to be on if there is no reference $id metadata in the JSON file?
+                ReferenceHandler = ReferenceHandler.Preserve,                                 
+            };
+            Options.Converters.Add(new JsonStringEnumConverter());
+        }        
+    }
+}
