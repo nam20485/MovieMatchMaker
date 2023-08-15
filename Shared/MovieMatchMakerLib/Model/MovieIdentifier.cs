103ea73df5a32d2e423aa458d3de3e39bb10c3f4
Nathan Miller <nmiller217@gmail.com>
2023-08-15 09:41:55 -0700

    add ! to allowable movie title characters and add custom YearRangeAttribute to  validate values respresenting years

--- Shared/MovieMatchMakerLib/Model/MovieIdentifier.cs
+++ Shared/MovieMatchMakerLib/Model/MovieIdentifier.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+
+using MovieMatchMakerLib.Utils;
 
 namespace MovieMatchMakerLib.Model
 {
     public class MovieIdentifier
     {
         [Required]
-        [StringLength(200)]
-        [RegularExpression("[A-Za-z0-9\\p{L}\\p{N} :']+")]   // > 0 Ascii letters & numbers, Unicode numbers and letters and space only
+        [StringLength(256)]     // longest existing movie title is documented as ~188 characters
+        [RegularExpression(@"[A-Za-z0-9\\p{L}\\p{N} :'\-!]+")]   // > 0 Ascii letters & numbers, Unicode numbers and letters and space only
         public string Title { get; set; }
 
         [Required]
-        [Range(1888, 2023)]
+        [YearRange()]           // [1, 2023] (earliest movie is documented as 1888)
         public int ReleaseYear { get; set; }
 
         public MovieIdentifier()
---
+++ Shared/MovieMatchMakerLib/Utils/YearRangeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieMatchMakerLib.Utils
+{
+    //[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+    //                AllowMultiple = false)]
+    public class YearRangeAttribute : RangeAttribute
+    {
+        /// <summary>
+        /// One parameter ctor specifies range: [minimum, DateTime.UtcNow.Year] (inclusive), i.e. up to current year
+        /// </summary>
+        /// <param name="minimum">minimum year, beginning of range (inclusive)</param>
+        public YearRangeAttribute(int minimum)
+            : base(minimum, DateTime.UtcNow.Year)
+        {
+        }
+
+        /// <summary>
+        /// No parameter ctor specifies range: [DateTime.MinValue.Year, DateTime.UtcNow.Year] (inclusive)
+        /// </summary>
+        public YearRangeAttribute()
+            : base(DateTime.MinValue.Year, DateTime.UtcNow.Year)
+        {
+        }
+
+        public YearRangeAttribute(int minimum, int maximum)
+            : base(minimum, maximum)
+        {
+        }
+
+        // not sure what this is for- disable it until its found to be needed
+        //public YearRangeAttribute([DynamicallyAccessedMembers((DynamicallyAccessedMemberTypes)(-1))] Type type,
+        //                          string minimum,
+        //                          string maximum)
+        //    : base(type, minimum, maximum)
+        //{
+        //}
+    }
+}
