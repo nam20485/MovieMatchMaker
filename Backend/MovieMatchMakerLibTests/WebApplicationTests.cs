7561976b7004cca39108fd8df1f29f9ba667234d
Nathan Miller <nmiller217@gmail.com>
2023-08-14 18:47:03 -0700

    add tests

--- Backend/MovieMatchMakerLibTests/WebApplicationTests.cs
+++ Backend/MovieMatchMakerLibTests/WebApplicationTests.cs
@@ -1,5 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using System;
+using System.Net;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Unicode;
 
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using MovieMatchMakerLib;
+using MovieMatchMakerLib.Model;
+
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MovieMatchMakerLibTests
 {
@@ -14,17 +26,22 @@ namespace MovieMatchMakerLibTests
         }
 
         [Theory]        
-        [InlineData("/api/MovieConnections/movieconnections")]
-        [InlineData("/api/MovieConnections/movieconnections/Dark%20City/1998")]
-        public async Task Test_Get_EndpointsReturnSuccessAndCorrectContentType_Urls(string url)
+        [InlineData("/api/MovieConnections/movieconnections", "application/json; charset=utf-8", HttpStatusCode.OK)]
+        [InlineData("/api/MovieConnections/movieconnections/Dark%20City/1998", "application/json; charset=utf-8", HttpStatusCode.OK)]        
+        [InlineData("/api/MovieConnections/movieconnections/graph/Dark%20City/1998", "text/plain; charset=utf-8", HttpStatusCode.OK)]
+        [InlineData("/api/MovieConnections/movieconnections/graph", "text/plain; charset=utf-8", HttpStatusCode.OK)]
+        [InlineData("/api/MovieConnections/movieconnection/0", "application/json; charset=utf-8", HttpStatusCode.OK)]
+        //[InlineData("/api/MovieConnections/movieconnection/In%20a%20Savage%20Land/1999/Dark%20City/1998", "application/json; charset=utf-8", HttpStatusCode.NoContent)]
+        public async Task Test_Get_EndpointsReturnSuccessAndCorrectContentType_Urls(string url, string contentType, HttpStatusCode statusCode)
         {            
             var client = _factory.CreateClient();         
             var response = await client.GetAsync(url);
 
-            response.IsSuccessStatusCode.Should().BeTrue();
+            //response.IsSuccessStatusCode.Should().BeTrue();
+            response.StatusCode.Should().Be(statusCode);
             response.Content.Headers.ContentType.Should().NotBeNull();
             response.Content.Headers.ContentType!.ToString().Should().NotBeEmpty();
-            response.Content.Headers.ContentType.ToString().Should().Be("application/json; charset=utf-8");          
+            response.Content.Headers.ContentType.ToString().Should().Be(contentType);          
         }
 
         [Theory]
@@ -33,11 +50,12 @@ namespace MovieMatchMakerLibTests
         [InlineData("/api-docs")]
         [InlineData("/api-docs/index.html")]
         public async Task Test_Get_EndpointsReturnSuccessAndCorrectContentType_Swagger(string url)
-        {
+        {            
             var client = _factory.CreateClient();
             var response = await client.GetAsync(url);
 
-            response.IsSuccessStatusCode.Should().BeTrue();
+            //response.IsSuccessStatusCode.Should().BeTrue();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
             response.Content.Headers.ContentType.Should().NotBeNull();
             response.Content.Headers.ContentType!.ToString().Should().NotBeEmpty();
             if (url == "/swagger" || url == "/swagger/index.html")
@@ -48,6 +66,27 @@ namespace MovieMatchMakerLibTests
             {
                 response.Content.Headers.ContentType.ToString().Should().StartWith("text/html");
             }
+        }
+
+        [Theory]
+        [InlineData("/api/MovieConnections/movieconnections")]
+        [InlineData("/api/MovieConnections/movieconnections/Dark%20City/1998")]
+        public async Task Test_AllMovieConnections_Deserialize(string url)
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.Content.Headers.ContentType.Should().NotBeNull();
+            response.Content.Headers.ContentType!.ToString().Should().NotBeEmpty();
+            response.Content.Headers.ContentType.ToString().Should().StartWith("application/json");
+
+            var strContent = await response.Content.ReadAsStringAsync();
+            strContent.Should().NotBeNull();
+            strContent.Should().NotBeEmpty();
+            var movieConnections = JsonSerializer.Deserialize<MovieConnection.List>(strContent);
+            movieConnections.Should().NotBeNull();
+            movieConnections.Should().NotBeEmpty();            
         }
     }
 }
