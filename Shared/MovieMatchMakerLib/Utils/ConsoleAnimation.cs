bf772ed0f172e91bd018f2517fbfc4e694387963
Nathan Miller <nmiller217@gmail.com>
2023-09-04 11:24:51 -0700

    refactor ConsoleAnimation

--- Shared/MovieMatchMakerLib/Utils/ConsoleAnimation.cs
+++ Shared/MovieMatchMakerLib/Utils/ConsoleAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 
 namespace MovieMatchMakerLib.Utils
@@ -12,11 +13,11 @@ namespace MovieMatchMakerLib.Utils
         public int Top { get; private set; }
         public int Left { get; private set; }
 
-        public int FrameDelayMs {  get; set; }
+        public int FrameDelayMs { get; set; }
         public bool HideCursor { get; set; }
         public bool Clear { get; set; }
-        public string LastFrame { get; set; }
-        
+        public string LastFrame { get; set; }        
+
         public ConsoleColor ForegroundColor
         {
             get => Console.ForegroundColor;
@@ -31,14 +32,15 @@ namespace MovieMatchMakerLib.Utils
         private readonly GetFrameTextFunc _getFrameTextFunc;
         private readonly Thread _thread;
 
-        private readonly ConsoleColors _consoleColors;        
-        
+        private readonly ConsoleColors _consoleColors;
+
         private volatile bool _stop = false;
         private uint _frameNumber;
         private int _longestFrameText;
+        private int _mostNumberOfLines;
         private (int Left, int Top) _prevPosition;
 
-        public ConsoleAnimation(int left, int top, GetFrameTextFunc getFrameTextFunc)            
+        public ConsoleAnimation(int left, int top, GetFrameTextFunc getFrameTextFunc)
         {
             Top = top;
             Left = left;
@@ -49,54 +51,38 @@ namespace MovieMatchMakerLib.Utils
             _prevPosition = Console.GetCursorPosition();
             _frameNumber = 0;
             _longestFrameText = 0;
+            _mostNumberOfLines = 1;
             _consoleColors = new ConsoleColors();
             FrameDelayMs = DefaultDelayMs;
             HideCursor = true;
-            Clear = false;                
+            Clear = false;
+            LastFrame = "";           
 
             ForegroundColor = Console.ForegroundColor;
-            BackgroundColor = Console.BackgroundColor;
+            BackgroundColor = Console.BackgroundColor;          
         }
 
         public ConsoleAnimation(GetFrameTextFunc getFrameTextFunc)
             : this(-1, -1, getFrameTextFunc)
         {
-        }      
-
-        private string LongestFrameLengthBlank() => new (' ', _longestFrameText+1);
-
-        public void Dispose()
-        {            
-            Stop();
-            _consoleColors.Dispose();
-            GC.SuppressFinalize(this);
         }
 
         public void Start()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             if (HideCursor)
             {
                 Console.CursorVisible = false;
             }
 
             // if no Left, Top provided, then use the current position (i.e. just after the last Console.Write)
-            if (Left == -1 &&
-                Top == -1)
+            if (Left == -1 && Top == -1)
             {
                 (Left, Top) = Console.GetCursorPosition();
             }
 
             _thread.Start();
-        }        
-
-        private void SavePosition()
-        {
-            _prevPosition = Console.GetCursorPosition();
-        }
-
-        public void ResetPosition()
-        {
-            Console.SetCursorPosition(_prevPosition.Left, _prevPosition.Top);
         }
 
         public void Stop()
@@ -116,42 +102,53 @@ namespace MovieMatchMakerLib.Utils
 
             if (!string.IsNullOrWhiteSpace(LastFrame))
             {
-                EraseFrame();
                 DrawFrame(LastFrame);
             }
 
             ResetPosition();
-            
+
             if (HideCursor)
             {
                 Console.CursorVisible = true;
             }
         }
 
-        private void EraseFrame()
-        {            
-            DrawFrame(LongestFrameLengthBlank());            
+        private (int, int) DrawFrame(string frameText)
+        {
+            for (int i = 0; i < _mostNumberOfLines; i++)
+            {
+                Console.WriteLine(LongestFrameLengthBlank);
+            }
+            Console.SetCursorPosition(Left, Top);
+            Console.Write(frameText);
+            return (frameText.Length, CountLines(frameText));
         }
 
-        private void DrawFrame(string frameText)
+        private static int CountLines(string text)
         {            
-            Console.Write(LongestFrameLengthBlank());
-            Console.SetCursorPosition(Left, Top);                                 
-            Console.Write(frameText);            
+            return text.Count(c => c.Equals(Environment.NewLine));
         }
 
         private void DrawFrameLoop()
         {
-            while (! _stop)
+            while (!_stop)
             {
-                var ft = GetFrameText(_frameNumber++);
-                DrawFrame(ft);
-                // capture longest frame text length for erasing when Stop()'ing
-                if (ft.Length > _longestFrameText)
-                {
-                    _longestFrameText = ft.Length;
+                try
+                {                    
+                    (int written, int lines) = DrawFrame(GetFrameText(_frameNumber++));
+                    // capture longest frame text length for erasing when Stop()'ing
+                    if (written > _longestFrameText ||
+                        lines > _mostNumberOfLines)
+                    {
+                        _longestFrameText = written;
+                        _mostNumberOfLines = lines;
+                    }
+                    Thread.Sleep(FrameDelayMs);
                 }
-                Thread.Sleep(FrameDelayMs);
+                catch (Exception e)
+                {
+                    DrawFrame(e.ToString()+Environment.NewLine);
+                }
             }
         }
 
@@ -159,6 +156,33 @@ namespace MovieMatchMakerLib.Utils
         protected virtual string GetFrameText(uint frameNumber)
         {
             return _getFrameTextFunc(frameNumber);
+        }
+
+        private void EraseFrame()
+        {
+            for (int i = 0; i < _mostNumberOfLines; i++)
+            {
+                Console.WriteLine(LongestFrameLengthBlank);
+            }
+        }
+
+        private string LongestFrameLengthBlank => new(' ', _longestFrameText + 1);
+
+        public void Dispose()
+        {
+            Stop();
+            _consoleColors.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        private void SavePosition()
+        {
+            _prevPosition = Console.GetCursorPosition();
+        }
+
+        public void ResetPosition()
+        {
+            Console.SetCursorPosition(_prevPosition.Left, _prevPosition.Top);
         }
     }
 }
